cb854f132409b931716220d79875565a84fb0bc0
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2014-10-07 12:46:05 +0800

    add nginx conf for twitter.com;

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ GOOGLE_HOST=mygoogle.example.com
 GOOGLE_SSL_CRT=/etc/nginx/certs/my.crt
 GOOGLE_SSL_KEY=/etc/nginx/certs/my.key
 
+TWITTER_HOST=mytwitter.example.com
+TWITTER_SSL_CRT=/etc/nginx/certs/tw.crt
+TWITTER_SSL_KEY=/etc/nginx/certs/tw.key
+
 SCRIPTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GEN=$SCRIPTDIR/generated
 
@@ -15,3 +19,9 @@ sed \
   -e "s#{{SSL_CRT}}#$GOOGLE_SSL_CRT#g" \
   -e "s#{{SSL_KEY}}#$GOOGLE_SSL_KEY#g" \
   $SCRIPTDIR/google.conf > $GEN/google.conf
+
+sed \
+  -e "s#{{HOST}}#$TWITTER_HOST#g" \
+  -e "s#{{SSL_CRT}}#$TWITTER_SSL_CRT#g" \
+  -e "s#{{SSL_KEY}}#$TWITTER_SSL_KEY#g" \
+  $SCRIPTDIR/twitter.conf > $GEN/twitter.conf
